5371d886189110b9ccf7625ceb32a04cbaa5a9ee
swathi-m-r <swathimr178@gmail.com>
2023-07-20 23:47:22 +0530

    removed console

--- financial-instruments/src/components/table/Table.js
+++ financial-instruments/src/components/table/Table.js
@@ -7,8 +7,6 @@ import './Table.css'
 const Table = () => {
     const [tableData, setTableData] = useState(financialInstrumentList);
     const [sortOrder, setSortOrder] = useState('asc')
-    const [ticketOrder, setTicketOrder] = useState('asc')
-    console.log("tabledata", tableData)
 
     const columns = [
         { label: "Ticket", dataKey: "ticker" },
@@ -33,46 +31,49 @@ const Table = () => {
     /**
      * Method to sort Ticket
      */
-    const sortTicket = () => {     
+    const sortTicket = () => {
         let newData = tableData.sort((a, b) => {
-                    if (a?.ticker > b?.ticker)
-                        return 1
-                    if (a?.ticker < b?.ticker)
-                        return -1
-                    return 0
-                })
+            if (a?.ticker > b?.ticker)
+                return 1
+            if (a?.ticker < b?.ticker)
+                return -1
+            return 0
+        })
         setTableData(newData)
-        
+
     }
 
-/**
- * Methos to sort asset class in order Equity , Macro and credit
- */
+    /**
+     * Methos to sort asset class in order Equity , Macro and credit
+     */
 
-const sortAssetClass = () => {
-    let equitiesArray = []
-    let creditArray =[]
-    let MacroArray = []
-    tableData.forEach(data=> {
-        if(data?.assetClass==='Equities'){
-            equitiesArray.push(data)
-        }else if(data?.assetClass==='Credit'){
-            creditArray.push(data)
-        }else{
-            MacroArray.push(data)
-        }
-    })
+    const sortAssetClass = () => {
+        let equitiesArray = []
+        let creditArray = []
+        let MacroArray = []
+        tableData.forEach(data => {
+            if (data?.assetClass === 'Equities') {
+                equitiesArray.push(data)
+            } else if (data?.assetClass === 'Credit') {
+                creditArray.push(data)
+            } else {
+                MacroArray.push(data)
+            }
+        })
 
-    const resultData = equitiesArray.concat(MacroArray,creditArray)
-    setTableData(resultData)
+        const resultData = equitiesArray.concat(MacroArray, creditArray)
+        setTableData(resultData)
 
-}
+    }
+    /**
+     * Method to handle sorting
+     */
     const handleSorting = (dataKey) => {
         if (dataKey === 'price') {
             sortPrice()
         } else if (dataKey === 'ticker') {
             sortTicket()
-        }else{
+        } else {
             sortAssetClass()
         }
     }
